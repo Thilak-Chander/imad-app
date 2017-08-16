50c3ec10deffd6bdfae9884d21f469f3f2fa56e9
Thilak-Chander <31039272+Thilak-Chander@users.noreply.github.com>
2017-08-16 22:34:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title:'Article One | Thilak Chander',
+    heading:'Article One',
+    date:'16 August 2017',
+    content:'<p>This is my content served by Article One.</p>'
+};
+function createTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+var htmlTemplate ='<html><head><title>${title}</title><meta name="viewport" content="width=device-width, initial-scale=1"/><link href="/ui/style.css" rel="stylesheet" /></head><body><div class="mass"><div><a href="/">Home</a></div><div><h3>${heading}</h3></div><div><p>${date}</p></div><div>${content}</div></div></body></html>';
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
