9431d755d49541b74d5963672a338f740acf7bb8
Thilak-Chander <31039272+Thilak-Chander@users.noreply.github.com>
2017-08-19 13:19:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 
 var img=document.getElementById('img');
-var marginLeft=0;
+var marginLeft = 0;
 function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginleft+'px';
+    marginLeft = marginLeft+1;
+    img.style.marginLeft = marginleft+'px';
 }
 img.onclick=function(){
-  var interval=setInterval(moveRight,50);  
+  var interval = setInterval(moveRight,50);  
 };
